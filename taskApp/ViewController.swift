78038a7be59ef7511e45b5e7833dd965d31c3456
tomoya.seki <te4090st@gmail.com>
2021-01-01 09:37:42 +0900

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by 関智矢 on 2021/01/01.
+//  Copyright © 2021 tomoya.seki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
